25e6935dc8503900b05a41352d04fb857f0a37a5
Hozeeben <hoze1995@yahoo.de>
2020-03-26 11:45:32 +0100

    Added vertical mirror (maybe finished)

--- MatrixPatternsearch.py
+++ MatrixPatternsearch.py
@@ -5,9 +5,13 @@ from mido import MidiFile
 
 def PatternSearch(track):
     notematrix = np.zeros((len(track)+1, len(track)+1))                       # Because [0][0] is not usable
+    differencematrix = np.zeros((len(track)+1, len(track)+1))
+    dimension = len(track)+1
     for itteration in range(1, len(track)+1):                                   # initialize Matrix
         notematrix[0, itteration] = track[itteration-1]
         notematrix[itteration, 0] = track[itteration-1]
+        differencematrix[0, itteration] = track[itteration - 1]
+        differencematrix[itteration, 0] = track[itteration - 1]
 
     foundpatternlength = []
     foundpatternindize = []                                                         # ToDo: Unterteilung der Algorithmen mit Switch Case
@@ -16,6 +20,11 @@ def PatternSearch(track):
     difference = []
     for i in range(0, len(track)-1):
         difference.append(track[i]-track[i+1])
+
+    for y in range (1, dimension):                                                 # Matrix for Keychange
+        for x in range (1, dimension):
+            differencematrix[y, x] = (track[y - 1]+track[x - 1])//2
+
     while True:                                                                     # Add Patterns here
         if yindize >= len(track)+1:                                                 # In-Range checks
             break
@@ -26,22 +35,40 @@ def PatternSearch(track):
         if xindize <= yindize:
             xindize += 1
             continue
-        if notematrix[yindize, xindize] == -1:
+
+        if notematrix[yindize, xindize] == -1:                                              #Patternclass-Keychange (Keychange in previous note found)
             notematrix[yindize, xindize] = notematrix[yindize - 1, xindize - 1] + 1
-        if notematrix[0, xindize] == notematrix[yindize, 0]:                        #Patternclass=same
+        if notematrix[0, xindize] == notematrix[yindize, 0]:                                #Patternclass=same
             if yindize != 1:
                 notematrix[yindize, xindize] = notematrix[yindize-1, xindize-1] + 1
             else:
                 notematrix[yindize, xindize] = 1
             xindize += 1
             continue
-        if xindize + 1 < np.size(notematrix, 1):                                    #Patternclass=Keychange
+
+        if xindize + 1 < dimension:                                            #Patternclass=Keychange
             if difference[yindize-1] == notematrix[0, xindize] - notematrix[0, xindize + 1]:
                 if yindize != 1:
                     notematrix[yindize, xindize] = notematrix[yindize - 1, xindize - 1] + 1
                 else:
                     notematrix[yindize, xindize] = 1
                 notematrix[yindize + 1, xindize + 1] = -1
+                xindize += 1
+                continue
+
+        if yindize + 2 < dimension and xindize + 2 < dimension:    #Pattermclass=vertical mirror
+            if differencematrix[yindize, xindize] == differencematrix[yindize + 1, xindize + 1] and differencematrix[yindize, xindize] == differencematrix[yindize + 2, xindize + 2]:
+                if yindize == 1:
+                    notematrix[yindize, xindize] = 1
+                else:
+                    notematrix[yindize, xindize] = notematrix[yindize - 1, xindize - 1]
+        else:
+            if xindize + 2 == dimension and yindize - 1 > 0:
+                if differencematrix[yindize - 1, xindize - 1] == differencematrix[yindize, xindize] and differencematrix[yindize - 1, xindize - 1] == differencematrix[yindize + 1, xindize + 1]:
+                    notematrix[yindize, xindize] = notematrix[yindize - 1, xindize - 1] + 1
+            elif xindize + 1 == dimension and yindize - 2 > 0:
+                if differencematrix[yindize - 2, xindize - 2] == differencematrix[yindize, xindize] and differencematrix[yindize - 2, xindize - 2] == differencematrix[yindize - 1, xindize - 1]:
+                    notematrix[yindize, xindize] = notematrix[yindize - 1, xindize - 1] + 1
         xindize += 1
 
 
@@ -65,7 +92,37 @@ def PatternSearch(track):
                     patternlength = 0
     return notematrix, foundpatternlength, foundpatternindize
 
+def ChangeNumbersToNotes(note):
+    noteswrite = ''
+    oktave = int((note // 12) - 1)
+    note = note % 12
+    oktave = str(oktave)
+    if note == 0:
+        noteswrite = 'C' + oktave
+    elif note == 1:
+        noteswrite = 'CIS' + oktave
+    elif note == 2:
+        noteswrite = 'D' + oktave
+    elif note == 3:
+        noteswrite = 'DIS' + oktave
+    elif note == 4:
+        noteswrite = 'E' + oktave
+    elif note == 5:
+        noteswrite = 'F' + oktave
+    elif note == 6:
+        noteswrite = 'FIS' + oktave
+    elif note == 7:
+        noteswrite = 'G' + oktave
+    elif note == 8:
+        noteswrite = 'GIS' + oktave
+    elif note == 9:
+        noteswrite = 'A' + oktave
+    elif note == 10:
+        noteswrite = 'AIS' + oktave
+    elif note == 11:
+        noteswrite = 'B' + oktave
 
+    return noteswrite
 
 if __name__ == '__main__':
     tempsting = 'note='
@@ -97,7 +154,7 @@ if __name__ == '__main__':
                 notematrix, patternlength, indize = PatternSearch(track)
                 sys.stdout = e
                 print('===Track Nr. ', nroftracks, '===')
-                for i in range(0, len(indize)):  # ToDo: nicht alle Patternergebnisse mit in .txt übernehmen
+                for i in range(0, len(indize)):
                     patternstring1 = ''
                     patternstring2 = ''
                     indizes = indize[i]
@@ -105,11 +162,11 @@ if __name__ == '__main__':
                     ytemp = int(float(tempstr[0]))
                     xtemp = int(float(tempstr[1]))
                     patternlengthtmp = int(patternlength[i])
-                    if ytemp + patternlengthtmp > xtemp:
+                    if ytemp + patternlengthtmp > xtemp:                            #Pattern in own pattern?
                         continue
                     for j in range(0, patternlengthtmp):
-                        patternstring1 = patternstring1 + ' ' + str(notematrix[ytemp + j, 0])
-                        patternstring2 = patternstring2 + ' ' + str(notematrix[xtemp + j, 0])
+                        patternstring1 = patternstring1 + ' ' + ChangeNumbersToNotes(notematrix[ytemp + j, 0])
+                        patternstring2 = patternstring2 + ' ' + ChangeNumbersToNotes(notematrix[xtemp + j, 0])
                     print('\nGefundene Pattern: ' + patternstring1 + ', ' + patternstring2 + '\nNoten im Lied: ' + str(ytemp) + ', ' + str(xtemp)
                           + '\nLänge des Patterns: ' + str(patternlengthtmp))
                 sys.stdout = normalstdout
