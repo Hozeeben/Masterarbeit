dc07eeea36674c7b97d6bfe7cbe87ae647adb1d9
Hozeeben <hoze1995@yahoo.de>
2020-04-08 19:25:05 +0200

    Worked on SIATEC.py (Finished Init)

--- SIATEC.py
+++ SIATEC.py
@@ -5,25 +5,35 @@ import matplotlib.pyplot as plt
 from mido import MidiFile
 
 def SIATEC(notey, timex):
-    notematrix = [[]]                                  # 3 dimensions because Python sucks
-    temparray = []
+    notematrix = [[[]]]                                  # 3 dimensions because Python sucks
+    temparray = [[]]
+    vektor = []
     vektorinformation = [0]
     vektorinformation.append(0)
-    for fromnote in range(0, len(notey)-1):
-        for tonote in range(0, len(notey)-1):
-            if fromnote <= tonote:
+    for tonote in range(0, len(notey)-1):
+        for fromnote in range(0, len(notey)-1):
+            if tonote <= fromnote:
                 while len(temparray) < len(notey) - 1:
-                    temparray.append('0')
+                    vektor.append('0, 0')
+                    temparray.append(vektor)
+                    vektor = []
                 notematrix.append(temparray)
                 temparray = []
                 break
             else:
-                vektorinformation[0] = int(notey[fromnote] - notey[tonote])
-                vektorinformation[1] = int(timex[fromnote] - timex[tonote])
-                temparray.append(', '.join(str(x) for x in vektorinformation))
+                vektorinformation[0] = int(notey[tonote] - notey[fromnote])
+                vektorinformation[1] = int(timex[tonote] - timex[fromnote])
+                vektor.append(', '.join(str(x) for x in vektorinformation))
+                temparray.append(vektor)
+                vektor = []
+    notematrix.pop(0)
     notematrix.pop(0)
 
-    # Sortieren und dann noch Pattern auslesen
+    # Sortieren und dann noch Pattern auslesen x-Achse ist welche Note gerade betrachtet wird und y-Achse zu welcher Note gegangen wird
+    # Bsp.: momentane Note     = 0,40
+    #       zu Note            = 1,47
+    #       Ermittelter Vektor = 1,7
+    # print(notematrix[5][3])   Erste Element Zeile, zweite Element Spalte
 
     plt.scatter(timex, notey)
     plt.show()
